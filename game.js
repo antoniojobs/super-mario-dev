284ce34afab72a1c33bd1293c4e594890f79afae
antonio.jobs <antonio.jjobs@gmail.com>
2021-05-12 16:07:09 -0300

    montagem chao e mario

--- game.js
+++ game.js
@@ -1,40 +1,92 @@
 console.log('Super-mario-dev version 0.0.1 ™ ®');
 
-const sprites = new Image();
-sprites.src = './assets/sprites/smw_mario_sheet.png';
+const spritesMap = new Image();
+spritesMap.src = './assets/sprites/mapa-1.png';
+// spritesMap.src = './assets/sprites/mapa-1.svg';
 const canvas = document.querySelector('canvas');
 const contexto =  canvas.getContext('2d');
 
-const resize = 2;
-const spaceA = 25;
-const spaceB = 19;
-
+/** */
+//tamanhos personagem
+let larguraMario=16;
+let alturaMario=28;
+let distanciaEntre=25+larguraMario;
+//tamanhos tela
+// width="510" height="446"
 const mario = {
-    sprites,
-    sx: 49, //sx, sy inicio da imagem
-    sy: 0,
-    sWidth:15, //sW , sH largura e altura
-    sHeight:20,
-    dx:28, // dx, dy coordenadas iniciar do desenho no canvas
-    dy:25,
-    dWidth:15, // tamanho do sprite
-    dHeight:20,
+    spritesMap,
+    coordenadaXSprite: 303, //coordenadaXSprite, sy inicio da imagem
+    coordenadaYSprite: 0,
+    larguraRecorte:larguraMario, //sW , sH largura e altura
+    alturaRecorte:alturaMario,
+    posicaoXMapa:50, // posicaoXMapa, dy coordenadas iniciar do desenho no canvas
+    posicaoYMapa:336,
+    dWidth:larguraMario, // tamanho do sprite
+    dHeight:alturaMario,
     desenha(){
         contexto.drawImage(
-            sprites,
-            mario.sx, mario.sy, //sx, sy inicio da imagem
-            mario.sWidth, mario.sHeight, //sW , sH largura e altura
-            mario.dx, mario.dy, // dx, dy coordenadas iniciar do desenho no canvas
-            (mario.dWidth)*resize, (mario.dHeight)*resize, // tamanho do sprite
+            spritesMap,
+            mario.coordenadaXSprite, mario.coordenadaYSprite, //sx, coordenadaYSprite inicio da imagem
+            mario.larguraRecorte, mario.alturaRecorte, //sW , sH largura e altura
+            
+            mario.posicaoXMapa, mario.posicaoYMapa, // dx, dy coordenadas iniciar do desenho no canvas
+            
+            mario.dWidth, mario.dHeight, // tamanho do sprite
+        );
+    }
+}
+
+const fundo = {
+    spritesMap,
+    coordenadaXSprite: 0, //coordenadaXSprite, sy inicio da imagem
+    coordenadaYSprite: 0,
+    larguraRecorte:255, //sW , sH largura e altura
+    alturaRecorte:236,
+    posicaoXMapa:0, // posicaoXMapa, dy coordenadas iniciar do desenho no canvas
+    posicaoYMapa:0,
+    dWidth:255*2, // tamanho do sprite
+    dHeight:236*2,
+    desenha(){
+        contexto.drawImage(
+            spritesMap,
+            fundo.coordenadaXSprite, fundo.coordenadaYSprite, //sx, coordenadaYSprite inicio da imagem
+            fundo.larguraRecorte, fundo.alturaRecorte, //sW , sH largura e altura
+            
+            fundo.posicaoXMapa, fundo.posicaoYMapa, // dx, dy coordenadas iniciar do desenho no canvas
+            
+            fundo.dWidth, fundo.dHeight, // tamanho do sprite
+        );
+    }
+}
+
+const chao = {
+    spritesMap,
+    coordenadaXSprite: 0, //coordenadaXSprite, sy inicio da imagem
+    coordenadaYSprite: 191,
+    larguraRecorte:255, //sW , sH largura e altura
+    alturaRecorte:236,
+    posicaoXMapa:0, // posicaoXMapa, dy coordenadas iniciar do desenho no canvas
+    posicaoYMapa:355,
+    dWidth:255*2, // tamanho do sprite
+    dHeight:236*2,
+    desenha(){
+        contexto.drawImage(
+            spritesMap,
+            chao.coordenadaXSprite, chao.coordenadaYSprite, //sx, coordenadaYSprite inicio da imagem
+            chao.larguraRecorte, chao.alturaRecorte, //sW , sH largura e altura
+            
+            chao.posicaoXMapa, chao.posicaoYMapa, // dx, dy coordenadas iniciar do desenho no canvas
+            
+            chao.dWidth, chao.dHeight, // tamanho do sprite
         );
     }
 }
 
 function loop() {
-    mario.desenha();
-    mario.dx +=1;
+    mario.desenha() ;
+    // fundo.desenha() ;
+    chao.desenha() ;
     requestAnimationFrame(loop);
 }
-
 loop();
 // void ctx.drawImage(image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight);
